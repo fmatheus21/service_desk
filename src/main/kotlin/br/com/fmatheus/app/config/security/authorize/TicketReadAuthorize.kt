d59bec0b03d3c4a958faa6540980e86bf7782540
Fernando Matheus <fernando.matheuss@hotmail.com>
2025-04-05 19:36:50 -0300

    Remoção de classe

--- src/main/kotlin/br/com/fmatheus/app/config/security/authorize/TicketReadAuthorize.kt
+++
@@ -1,8 +0,0 @@
-package br.com.fmatheus.app.config.security.authorize
-
-import org.springframework.security.access.prepost.PreAuthorize
-
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@PreAuthorize("hasAnyAuthority('service_desk/all_authorize', 'service_desk/ticket_read', 'client_credentials')")
-annotation class TicketReadAuthorize()
